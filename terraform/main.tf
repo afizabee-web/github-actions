126c2e1688cec68dccfdb0e2f28db940916df959
Ubuntu <azureuser@VM-A.zh1ywasisvke1lrtvf32cld0bf.rx.internal.cloudapp.net>
2025-12-29 07:40:54 +0000

    updated sku

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,7 +24,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "system"
     node_count = 1
-    vm_size    = "Standard_DS2_v2"
+    vm_size    = "standard_d2ds_v5"
   }
 
   identity {
