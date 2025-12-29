923cd015d1435188a9b982b56b99b1acbcd2106d
Ubuntu <azureuser@VM-A.zh1ywasisvke1lrtvf32cld0bf.rx.internal.cloudapp.net>
2025-12-29 07:00:07 +0000

    all changes to new directory

---
+++ terraform/main.tf
@@ -0,0 +1,34 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "AKS-RG-New"
+  location = "Central India"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "ghademoacr123"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-gha-demo"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aksdemo"
+
+  default_node_pool {
+    name       = "system"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
