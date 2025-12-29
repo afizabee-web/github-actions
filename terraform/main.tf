1e180d64cf0b150bc2d07a9ffd8d1ca824025982
Ubuntu <azureuser@VM-A.zh1ywasisvke1lrtvf32cld0bf.rx.internal.cloudapp.net>
2025-12-29 09:23:43 +0000

    modified main.tf to not to create resources if already exists

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "ghademotfstate123"
+    container_name       = "tfstate"
+    key                  = "github-actions/terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
   features {}
 }
