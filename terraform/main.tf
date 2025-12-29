0c4f18d6224107a1b967fe5671d91143347ecf5f
Ubuntu <azureuser@VM-A.zh1ywasisvke1lrtvf32cld0bf.rx.internal.cloudapp.net>
2025-12-29 09:32:25 +0000

    added lifecycle in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,33 +11,54 @@ provider "azurerm" {
   features {}
 }
 
+# ================================
+# EXISTING RESOURCE GROUP
+# ================================
 resource "azurerm_resource_group" "rg" {
-  name     = "AKS-RG-New1"
-  location = "Central India"
+  name     = "AKS-RG-New1"   # 👈 existing RG name
+  location = "East US"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
+# ================================
+# AZURE CONTAINER REGISTRY
+# ================================
 resource "azurerm_container_registry" "acr" {
   name                = "ghademoacr123"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
   admin_enabled       = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
+# ================================
+# AKS CLUSTER
+# ================================
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "aks-gha-demo"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "aksdemo"
+  dns_prefix          = "aksghademo"
 
   default_node_pool {
     name       = "system"
     node_count = 1
-    vm_size    = "standard_d2ds_v5"
+    vm_size    = "Standard_DS2_v2"
   }
 
   identity {
     type = "SystemAssigned"
+  }
+
+  lifecycle {
+    prevent_destroy = true
   }
 }
 
