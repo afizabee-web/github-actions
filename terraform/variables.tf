4caf92c66ffbaa87985dfadcf14c427338c2a92f
afizabee-web <afizabee365@gmail.com>
2026-01-01 20:03:01 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,31 +1,31 @@
 variable "subscription_id" {
-  description = "Azure subscription ID"
+  description = "c62d8ace-a447-4720-ae45-1e174ef20582"
   type        = string
 }
 
-variable "resource_group_name" {
-  description = "Existing Resource Group name"
+variable "resource_gAKS-RGroup_name" {
+  description = ""
   type        = string
 }
 
 variable "acr_name" {
-  description = "Existing Azure Container Registry name"
+  description = "acr1123"
   type        = string
 }
 
 variable "aks_name" {
-  description = "AKS cluster name"
+  description = "tf-cluster"
   type        = string
 }
 
 variable "node_count" {
-  description = "AKS node count"
+  description = "1"
   type        = number
   default     = 2
 }
 
 variable "node_vm_size" {
-  description = "AKS node VM size"
+  description = "Standard_D4ds_v5"
   type        = string
   default     = "Standard_DS2_v2"
 }
